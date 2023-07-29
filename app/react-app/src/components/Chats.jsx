c8ee01a676fcedd44b29b87e1dfa5872c43615e1
ttoda <t.taiga890@gmail.com>
2023-07-29 10:15:18 +0900

    Add Chats and Chat component

---
+++ app/react-app/src/components/Chats.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { Chat } from './index'
+import List from '@material-ui/core/List';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '36ch',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+const Chats = () => {
+  const classes = useStyles();
+  return(
+    <List className={classes.root}>
+      <Chat/>
+      <Chat/>
+      <Chat/>
+      <Chat/>
+    </List>
+  )
+}
+
+export default Chats
