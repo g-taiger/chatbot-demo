9904dbcdd5dd0acdd74e002d1200feef8ec3bf04
ttoda <t.taiga890@gmail.com>
2023-07-30 09:54:04 +0900

    Apply dataset to chat and chats components

--- app/react-app/src/components/Chat.jsx
+++ app/react-app/src/components/Chat.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
 
-const Chat = () => {
+const Chat = (props) => {
+  const isQusetion = (props.type === "question");
+  const classes = isQusetion ? "p-chat__row" : "p-chat__reverse";
+
   return(
-    <ListItem alignItems="flex-start">
+    <ListItem className={classes}>
         <ListItemAvatar>
           <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
         </ListItemAvatar>
-        <div className="p-chat__bubble">ダミーテキスト</div>
+        <div className="p-chat__bubble">{props.text}</div>
     </ListItem>
   )
 }
